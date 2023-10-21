a150829fc4bc57ba08473d9cfa2b48384eee1ced
Andrew Lyu <76072189+AAAAA11331@users.noreply.github.com>
2023-10-20 21:56:41 -0400

    Add files via upload

---
+++ messageServer.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void child(int);
+
+int main(int argc, char *argv[]){//E.g., 2: Server
+  int sd, client, portNumber;
+  socklen_t len;
+  struct sockaddr_in servAdd;
+  if(argc != 2){
+    printf("Call model: %s <Port #>\n", argv[0]);
+    exit(0);
+  }
+  if ((sd=socket(AF_INET,SOCK_STREAM,0))<0){
+    fprintf(stderr, "Cannot create socket\n");
+    exit(1);
+  }
+  servAdd.sin_family = AF_INET;
+  servAdd.sin_addr.s_addr = INADDR_ANY;
+  sscanf(argv[1], "%d", &portNumber);
+  servAdd.sin_port = portNumber;
+  bind(sd,(struct sockaddr*)&servAdd,sizeof(servAdd));
+  listen(sd, 5);
+  while(1){
+    client=accept(sd,(struct sockaddr*)NULL,NULL);
+    printf("Got a client\n");
+    if(!fork())
+      child(client);
+    close(client);
+  }
+}
+void child(int sd){
+  char message[255];
+  while(1){
+    fprintf(stderr, "Enter line to send to client\n");
+    fgets(message, 254, stdin);
+    write(sd, message, strlen(message)+1);
+    if(!read(sd, message, 255)){
+      close(sd);
+      fprintf(stderr,"Bye, client dead, wait for a new client\n");
+      exit(0);
+    }
+    fprintf(stderr, "Clent send back: %s\n", message);
+  }
+}
